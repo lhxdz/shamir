94d7c1ed5b335897479dda8fd8dfe8a934e27e72
745150400 <745150400@qq.com>
2022-11-10 00:44:41 +0800

    1. 新增加解密复合key 2. 新增加解密支持hash校验

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/json-iterator/go v1.1.12
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/olekukonko/tablewriter v0.0.5
+	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.6.1
 	github.com/spf13/viper v1.13.0
 	github.com/stretchr/testify v1.8.1
--- pkg/utils/shamir/encrypt.go
+++ pkg/utils/shamir/encrypt.go
@@ -1,11 +1,13 @@
 package shamir
 
 import (
+	"crypto/sha256"
 	"fmt"
 	"math"
 	"math/big"
 
 	"shamir/pkg/utils/code"
+	"shamir/pkg/utils/log"
 	math2 "shamir/pkg/utils/math"
 )
 
@@ -18,13 +20,63 @@ var minPrime = math2.NextPrime(big.NewInt(math.MaxInt64))
 // Encrypt shamir加密算法，输入秘密secret，门限值threshold，密钥个数keysNumber
 // 返回加密结果密钥对keys，和加密使用的素数，都需谨慎保存
 func Encrypt(secret *big.Int, threshold, keysNumber int, fast bool) (keys []code.Key, prime *big.Int, err error) {
-	if threshold > keysNumber {
-		return nil, nil, fmt.Errorf("threshold(%d) can not bigger than keys number(%d)", threshold, keysNumber)
-	}
-	if threshold < MinThreshold {
-		return nil, nil, fmt.Errorf("threshold(%d) can not smaller than %d", threshold, MinThreshold)
+	if err = encryptCheck(secret, threshold, keysNumber); err != nil {
+		return nil, nil, err
 	}
 
+	return encrypt(secret, threshold, keysNumber, fast)
+}
+
+// CompoundEncrypt 用于加密复合型秘密，由于秘密太大，拆分成多个子秘密加密，将会生成复合型密钥和复合型素数
+func CompoundEncrypt(secret []*big.Int, threshold, keysNumber int, fast bool) (keys []code.CompoundKey, prime []*big.Int, err error) {
+	if err = CompoundEncryptCheck(secret, threshold, keysNumber); err != nil {
+		return nil, nil, err
+	}
+
+	return compoundEncrypt(secret, threshold, keysNumber, fast)
+}
+
+// HashEncrypt 将秘密进行hash计算，并将计算结果一并加密进入密钥中
+func HashEncrypt(secret []*big.Int, threshold, keysNumber int, fast bool) (keys []code.CompoundKey, prime []*big.Int, err error) {
+	if err = CompoundEncryptCheck(secret, threshold, keysNumber); err != nil {
+		return nil, nil, err
+	}
+
+	hash := sha256.New()
+	for i := 0; i < len(secret); i++ {
+		_, err = hash.Write(secret[i].Bytes())
+		if err != nil {
+			log.Errorf("hash check sum failed: %v", err)
+			return nil, nil, HashCheckFailed
+		}
+	}
+
+	hashInt := code.EncodeSecret(string(hash.Sum(nil)))
+	// hash值的加密不使用fast，实时计算prime
+	hashKeys, hashPrime, err := encrypt(hashInt, threshold, keysNumber, false)
+	if err != nil {
+		log.Errorf("encrypt hash value failed: %v", err)
+		return nil, nil, err
+	}
+
+	keys, prime, err = compoundEncrypt(secret, threshold, keysNumber, fast)
+	if err != nil {
+		log.Errorf("hash encrypt failed: %v", err)
+		return nil, nil, err
+	}
+
+	prime = append(prime, hashPrime)
+	for i, tmpKey := range hashKeys {
+		keys[i].X = append(keys[i].X, tmpKey.X)
+		keys[i].Y = append(keys[i].Y, tmpKey.Y)
+	}
+
+	return
+}
+
+// private
+
+func encrypt(secret *big.Int, threshold, keysNumber int, fast bool) (keys []code.Key, prime *big.Int, err error) {
 	if minPrime.Cmp(secret) > 0 {
 		// 若秘密太小，则使用默认质数
 		prime = new(big.Int).Set(minPrime)
@@ -57,6 +109,56 @@ func Encrypt(secret *big.Int, threshold, keysNumber int, fast bool) (keys []code
 	}
 
 	return
+}
+
+func compoundEncrypt(secret []*big.Int, threshold, keysNumber int, fast bool) (keys []code.CompoundKey, prime []*big.Int, err error) {
+	keys = make([]code.CompoundKey, keysNumber, keysNumber)
+	prime = make([]*big.Int, 0, len(secret))
+
+	for _, tmpSecret := range secret {
+		tmpKeys, tmpPrime, e := encrypt(tmpSecret, threshold, keysNumber, fast)
+		if e != nil {
+			log.Errorf("compound encrypt failed: %v", e)
+			return nil, nil, e
+		}
+
+		prime = append(prime, tmpPrime)
+		for i, tmpKey := range tmpKeys {
+			keys[i].X = append(keys[i].X, tmpKey.X)
+			keys[i].Y = append(keys[i].Y, tmpKey.Y)
+		}
+	}
+
+	return
+}
+
+func encryptCheck(secret *big.Int, threshold, keysNumber int) error {
+	if secret == nil {
+		return fmt.Errorf("nil point of secret")
+	}
+
+	return tnCheck(threshold, keysNumber)
+}
+
+func CompoundEncryptCheck(secret []*big.Int, threshold, keysNumber int) error {
+	for _, tmpSecret := range secret {
+		if tmpSecret == nil {
+			return fmt.Errorf("nil point of secret")
+		}
+	}
+
+	return tnCheck(threshold, keysNumber)
+}
+
+func tnCheck(threshold, keysNumber int) error {
+	if threshold > keysNumber {
+		return fmt.Errorf("threshold(%d) can not bigger than keys number(%d)", threshold, keysNumber)
+	}
+	if threshold < MinThreshold {
+		return fmt.Errorf("threshold(%d) can not smaller than %d", threshold, MinThreshold)
+	}
+
+	return nil
 }
 
 // 计算 f(x) = (a0 + a1*(x^1) + a2*(x^2) + ... an*(x^n)) mod prime
