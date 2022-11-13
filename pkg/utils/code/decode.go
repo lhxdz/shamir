0f88b7601568d289fb660133c37ccc4306653fc2
745150400 <745150400@qq.com>
2022-11-14 00:58:54 +0800

    新增文件内容操作功能 1. 秘密输入流中挨个读取用于加密的大整数 2. 密钥输入流挨个读取子秘钥 3. 解密后秘密写到输出流 4. 加密后密钥挨个写入输出流

--- pkg/utils/code/decode.go
+++ pkg/utils/code/decode.go
@@ -2,6 +2,10 @@ package code
 
 import (
 	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"hash"
+	"io"
 	"math/big"
 )
 
@@ -47,6 +51,84 @@ func DecodeKeys(keys []*big.Int) string {
 	}
 
 	return result
+}
+
+type SecretDecoder struct {
+	// 用于记录最后一次传入的数据，因为最后一个是hash校验值
+	lastData []byte
+	hash     hash.Hash
+	writer   io.Writer
+}
+
+func NewSecretDecoder(writer io.Writer) *SecretDecoder {
+	hashWriter := sha256.New()
+	multiWriter := io.MultiWriter(writer, hashWriter)
+	return &SecretDecoder{
+		writer: multiWriter,
+		hash:   hashWriter,
+	}
+}
+
+func (s *SecretDecoder) Write(data *big.Int) error {
+	if data == nil || len(data.Bytes()) == 0 {
+		return fmt.Errorf("invalid data bit integer")
+	}
+
+	// 每次写上次的数据，防止将最后一个hash校验的数据误写入
+	if s.lastData != nil {
+		n, err := s.writer.Write(s.lastData)
+		if err != nil {
+			return fmt.Errorf("write data failed: %w", err)
+		}
+		if n != len(s.lastData) {
+			return fmt.Errorf("write data failed, expected write %d bytes, actual %d bytes", len(s.lastData), n)
+		}
+	}
+
+	// 将这次传入的数据保存
+	s.lastData = getSecretBytes(data)
+	return nil
+}
+
+func (s *SecretDecoder) HashCheck() error {
+	expectedHash := string(s.lastData)
+	actuallyHash := string(s.hash.Sum(nil))
+	if expectedHash != actuallyHash {
+		return fmt.Errorf("expected hash %s, actual hash %s", expectedHash, actuallyHash)
+	}
+
+	return nil
+}
+
+type KeyDecoder struct {
+	split  string
+	writer io.Writer
+}
+
+func NewKeyDecoder(writer io.Writer) *KeyDecoder {
+	return &KeyDecoder{
+		writer: writer,
+	}
+}
+
+func (k *KeyDecoder) Write(key *big.Int) error {
+	if key == nil {
+		return fmt.Errorf("%w, nil point", InvalidKey)
+	}
+
+	// 首次写入时前面没有分隔符
+	keyData := key.Append([]byte(k.split), base)
+	n, err := k.writer.Write(keyData)
+	if err != nil {
+		return fmt.Errorf("write key data failed: %w", err)
+	}
+	if n != len(keyData) {
+		return fmt.Errorf("write key data failed, expected write %d bytes, actual %d bytes", len(keyData), n)
+	}
+
+	// 首次写入后，添加分隔符
+	k.split = splitKey
+	return nil
 }
 
 // private
--- pkg/utils/code/encode.go
+++ pkg/utils/code/encode.go
@@ -1,14 +1,27 @@
 package code
 
 import (
+	"bufio"
+	"crypto/sha256"
+	"fmt"
+	"hash"
+	"io"
 	"math/big"
 	"strings"
+
+	"github.com/pkg/errors"
 
 	"shamir/pkg/utils/compute"
 )
 
 const (
 	bytePrefix = 0xff
+	// 目前最大分割的子秘密长1024，其生成的key不会超过2048长度
+	maxKeyLen = 2048
+)
+
+var (
+	InvalidKey = errors.New("invalid key")
 )
 
 // EncodeSecret 将字符串秘密编码成为大整数，方便加密
@@ -44,6 +57,85 @@ func EncodeKeys(key string) ([]*big.Int, bool) {
 	return bigIntegers, true
 }
 
+type SecretEncoder struct {
+	splitLen int
+	hash     hash.Hash
+	reader   io.Reader
+}
+
+func NewSecretEncoder(reader io.Reader, splitLen int) *SecretEncoder {
+	return &SecretEncoder{
+		splitLen: splitLen,
+		hash:     sha256.New(),
+		reader:   reader,
+	}
+}
+
+func (s *SecretEncoder) Read() (*big.Int, error) {
+	data := make([]byte, s.splitLen)
+	n, err := s.reader.Read(data)
+	if err != nil && !errors.Is(err, io.EOF) {
+		return nil, fmt.Errorf("read secret file failed: %w", err)
+	}
+
+	if n == 0 {
+		return nil, nil
+	}
+
+	nHash, err := s.hash.Write(data[:n])
+	if err != nil {
+		return nil, fmt.Errorf("secret hash check failed: %w", err)
+	}
+	if nHash != n {
+		return nil, fmt.Errorf("secret hash check failed, hash write expected %d bytes, actual %d bytes", n, nHash)
+	}
+
+	return new(big.Int).SetBytes(append([]byte{bytePrefix}, data[:n]...)), nil
+}
+
+func (s *SecretEncoder) GetHash() *big.Int {
+	return new(big.Int).SetBytes(s.hash.Sum(nil))
+}
+
+type KeyEncoder struct {
+	reader *bufio.Reader
+}
+
+func NewKeyEncoder(reader io.Reader) *KeyEncoder {
+	return &KeyEncoder{
+		reader: bufio.NewReaderSize(reader, maxKeyLen),
+	}
+}
+
+// Read 返回密钥，密钥类型(是否是hash值的密钥)
+func (s *KeyEncoder) Read() (*big.Int, bool, error) {
+	data, err := s.reader.ReadSlice(splitKey[0])
+	if err != nil && !errors.Is(err, io.EOF) {
+		// 正确的key的长度无法达到这么大
+		if errors.Is(err, bufio.ErrBufferFull) {
+			err = InvalidKey
+		}
+		return nil, false, fmt.Errorf("read key file failed: %w", err)
+	}
+
+	if len(data) == 0 {
+		return nil, errors.Is(err, io.EOF), InvalidKey
+	}
+
+	// 非hash值密钥
+	if !errors.Is(err, io.EOF) {
+		// 去除最后一个字符的分隔符
+		data = data[:len(data)-1]
+	}
+
+	result, err := encodeKey(data)
+	if err != nil {
+		return nil, errors.Is(err, io.EOF), err
+	}
+
+	return result, errors.Is(err, io.EOF), nil
+}
+
 // private
 
 func getBucketCounts(size, bucketSize int) int {
@@ -51,4 +143,16 @@ func getBucketCounts(size, bucketSize int) int {
 		return size / bucketSize
 	}
 	return size/bucketSize + 1
+}
+
+func encodeKey(data []byte) (*big.Int, error) {
+	if len(data) == 0 {
+		return nil, InvalidKey
+	}
+
+	result, ok := EncodeKey(string(data))
+	if !ok {
+		return nil, InvalidKey
+	}
+	return result, nil
 }
